675f134a2dac66496b84693682e7c54dd55db402
陈亚楠 <878493162@qq.com>
2021-03-09 10:01:46 +0800

    fixed: 获取rto组件的操作url

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -34,7 +34,6 @@ window.addEventListener('load', () => {
 
   // 监听组件选中
   window.SuanpanAPI.eventService.on('sp:node:select', (event, data) => {
-    console.log(event, data)
     if (data && data.length > 1) {
       const nodeInfo = data[1];
       //  判断当前选中组件是否为rto组件
@@ -48,16 +47,13 @@ window.addEventListener('load', () => {
   // 监听rto组件打开操作面板
   window.SuanpanAPI.eventService.on('rto:setting:params', (event, data) => {
     console.log(event, data)
-    if (data && data.length > 1) {
-      const nodeInfo = data[1];
-      const { actionList } = nodeInfo.metadata.def;
-      actionList.map(action => {
-        if (action.hasOwnProperty('label') && action.hasOwnProperty('url')) {
-          storeInst.commit('drawer/changeDrawerVisible', true);
-          storeInst.commit('drawer/changeIsModelAlgoManage', false);
-          storeInst.commit('drawer/changeIframURL', action.url);
-        }
-      });
+    if (data && data.length > 0 && data[0].hasOwnProperty('url')) {
+      const { url } = data[0];
+      if (url && url !== '') {
+        storeInst.commit('drawer/changeDrawerVisible', true);
+        storeInst.commit('drawer/changeIsModelAlgoManage', false);
+        storeInst.commit('drawer/changeIframURL', url);
+      }
     }
   });
 })
