c2e5766ccdffde3a230d6b538b31443a1a9fb1ed
xinyufyj <feiyujie007@sina.com>
2021-03-10 19:55:29 +0800

    app status

--- src/renderer/store/status.js
+++ src/renderer/store/status.js
@@ -4,8 +4,16 @@ export default {
     // "AppRunning": "1", "AppStopped": "2", "AppSuccess": "3", "AppFailed" : "4", 
     // "AppStarting": "5", "AppStopping": "6", "AppDead": "7", "AppCron": "8", 
     // "AppWaiting": "9", "AppUnknown": "0"
-    appStatus: 0,
+    appStatus: '0',
   },
   mutations: {},
-  actions: {},
+  actions: {
+    getStatus({ state, commit, rootState }) {
+      if(rootState.file.currentAppId) {
+        window.SuanpanAPI.predictService.getNetworkStatus(rootState.file.currentAppId).then( res => {
+          
+        })
+      }
+    }
+  },
 };
