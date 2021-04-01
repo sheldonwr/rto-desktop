2a0a34c5dacea0109ca06d6f6ddd4669a532f4dd
陈亚楠 <878493162@qq.com>
2021-04-01 17:17:51 +0800

    fixed: 算法管理和模型管理弹窗地址

--- src/renderer/components/Menu/index.vue
+++ src/renderer/components/Menu/index.vue
@@ -369,21 +369,13 @@ export default {
           // this.$store.commit("drawer/changeDrawerVisible", true);
           this.$store.commit("drawer/changeActiveTab", "model");
           this.$store.commit("drawer/changeIsModelAlgoManage", true);
-          // this.$store.dispatch("ci/getList", {
-          //   type: "model",
-          //   pagination: this.$store.getters["ci/getPagination"],
-          // });
-          window.open(`http://localhost:8080/modelAlgoManage`);
+          window.open(`${window.location.origin}/modelAlgoManage`);
           break;
         case "algo-manage": // 算法管理
           // this.$store.commit("drawer/changeDrawerVisible", true);
           this.$store.commit("drawer/changeActiveTab", "algo");
           this.$store.commit("drawer/changeIsModelAlgoManage", true);
-          this.$store.dispatch("ci/getList", {
-            type: "algo",
-            pagination: this.$store.getters["ci/getPagination"],
-          });
-          window.open(`${window.location.href}/modelAlgoManage`);
+          window.open(`${window.location.origin}/modelAlgoManage`);
           break;
       }
     },
