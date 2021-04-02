2b9610a8a957e94a400437c45695ccbdb62dfb66
xinyufyj <feiyujie007@sina.com>
2021-04-02 18:08:17 +0800

    running app table

--- src/renderer/components/Wizard.vue
+++ src/renderer/components/Wizard.vue
@@ -70,6 +70,9 @@
                 <a-badge
                   :status="status === 'Running' ? 'success' : 'default'"
                 ></a-badge>{{ status === 'Running' ? '运行中' : '已停止' }}</span>
+              <span slot="operation" slot-scope="item">
+                <a href="#" @click="enterApp({id:item.appId, name:item.appName})">查看</a>
+              </span>
             </a-table>
           </div>
         </div>
@@ -131,12 +134,19 @@ export default {
         {
           title: "CPU占用(毫核)",
           dataIndex: "resources.cpu",
-          key: "resources.cpu"
+          key: "resources.cpu",
+          align: 'center'
         },
         {
           title: "内存占用(M)",
           dataIndex: "resources.mem",
           key: "resources.mem",
+          align: 'center'
+        },
+        {
+          title: '操作',
+          key: "operation",
+          scopedSlots: { customRender: 'operation' },
         },
       ],
     };
